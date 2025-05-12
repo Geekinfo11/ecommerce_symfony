95e343f0303e4677bf837918e93786bedd9e85d9
Hassan AMAAMER <hassan.amaamer@gmail.com>
2025-05-12 17:01:09 +0100

    add logic to register users to db, hash password before persist to db, adjust errors styling

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManagerInterface): Response
     {
-        $form = $this->createForm(RegisterUserTypeForm::class);
+        // create a new empty User object to be filled with form data upon form submission
+        $user = new User();
+
+        // create a form instance using the RegisterUserTypeForm class, which defines the form's fields and validation rules, and bind it to the User object
+        $form = $this->createForm(RegisterUserTypeForm::class, $user);
+
+        // handles the request: if it's a POST (form submitted), it populates the form with submitted data from request then $user object will be automatically populated with the form data
+        $form->handleRequest($request);
+
+        // if the form is submitted and valid, persist the user to the database
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+        }
 
         return $this->render('register/index.html.twig', [
             'registerForm' => $form
--- src/Form/RegisterUserTypeForm.php
+++ src/Form/RegisterUserTypeForm.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,12 +29,25 @@ class RegisterUserTypeForm extends AbstractType
                 'attr' => ['placeholder' => 'Enter your email'],
                 'label' => 'Email'
             ])
-            ->add('password', PasswordType::class, [
-                'attr' => ['placeholder' => 'Enter you password'],
-                'label' => 'Password'
+            // use RepeatedType to create two password fields: one for entry and one for confirmation
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => ['placeholder' => 'Enter your password', 'class' => 'mb-1'],
+                    'hash_property_path' => 'password' // the password will be automatically hashed and stored in the 'password' property of the object
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => ['placeholder' => 'Repeat your password'],
+                ],
+                // prevent automatic mapping. This means Symfony will not attempt to map the "plainPassword" form field 
+                // to a non-existent "plainPassword" property in the User entity.
+                'mapped' => false,
             ])
             ->add('submit', SubmitType::class, [
-                'attr' => ['class' => 'btn btn-primary']
+                'attr' => ['class' => 'btn btn-success'],
+                'label' => 'Register'
             ])
         ;
     }

64054560e28418bc945739ee13457c933fbca311
Hassan AMAAMER <hassan.amaamer@gmail.com>
2025-05-12 18:44:35 +0100

    add form validation & adjust register page styling

--- src/Form/RegisterUserTypeForm.php
+++ src/Form/RegisterUserTypeForm.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserTypeForm extends AbstractType
 {
@@ -19,23 +22,42 @@ class RegisterUserTypeForm extends AbstractType
         $builder
             ->add('first_name', TextType::class, [
                 'attr' => ['placeholder' => 'Enter your first name'],
-                'label' => 'First name'
+                'label' => 'First name',
+                // add validations
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 50
+                    ])
+                ]
             ])
             ->add('last_name', TextType::class, [
                 'attr' => ['placeholder' => 'Enter your last name'],
-                'label' => 'Last name'
+                'label' => 'Last name',
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'max' => 50
+                    ])
+                ]
             ])
             ->add('email', EmailType::class, [
                 'attr' => ['placeholder' => 'Enter your email'],
-                'label' => 'Email'
+                'label' => 'Email',
+                'constraints' => [
+                    new Email([
+                        'message' => 'The email "{{value}}" is not a valid email.'
+                    ])
+                ]
             ])
             // use RepeatedType to create two password fields: one for entry and one for confirmation
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options'  => [
                     'label' => 'Password',
-                    'attr' => ['placeholder' => 'Enter your password', 'class' => 'mb-1'],
-                    'hash_property_path' => 'password' // the password will be automatically hashed and stored in the 'password' property of the object
+                    'attr' => ['placeholder' => 'Enter your password'],
+                    'hash_property_path' => 'password', // the password will be automatically hashed and stored in the 'password' property of the object
+
                 ],
                 'second_options' => [
                     'label' => 'Repeat Password',
@@ -44,6 +66,12 @@ class RegisterUserTypeForm extends AbstractType
                 // prevent automatic mapping. This means Symfony will not attempt to map the "plainPassword" form field 
                 // to a non-existent "plainPassword" property in the User entity.
                 'mapped' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 8,
+                        'max' => 50
+                    ])
+                ]
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'btn btn-success'],
@@ -56,6 +84,10 @@ class RegisterUserTypeForm extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            // prevent registration of a new user if the email already exists in the database
+            'constraints' => [
+                new UniqueEntity(fields: ['email']),
+            ],
         ]);
     }
 }
